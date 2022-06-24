bc46cbce0e890b4e13059e08ff9cdbc9c76d2928
hyeongsoo <hyeongsoo@postech.ac.kr>
2022-06-24 23:31:28 +0900

    commit

--- demo_app/lib/main.dart
+++ demo_app/lib/main.dart
@@ -13,18 +13,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter App!',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          ),
+      home: const MyHomePage(title: '데모 앱입니다'),
     );
   }
 }
