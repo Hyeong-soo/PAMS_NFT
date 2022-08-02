fa4c82174627065004126eb5b8907191d1799f09
eun sung cha <105369802+silca3553@users.noreply.github.com>
2022-08-03 01:37:24 +0900

    Delete hello_world directory

--- hello_world/target/debug/deps/hello_world-d9a5d4e6c1bae75f.d
+++
@@ -1,5 +0,0 @@
-c:\Users\progr\OneDrive - postech.ac.kr\PDAO\near\hello_world\target\debug\deps\hello_world-d9a5d4e6c1bae75f.rmeta: src\main.rs
-
-c:\Users\progr\OneDrive - postech.ac.kr\PDAO\near\hello_world\target\debug\deps\hello_world-d9a5d4e6c1bae75f.d: src\main.rs
-
-src\main.rs:
